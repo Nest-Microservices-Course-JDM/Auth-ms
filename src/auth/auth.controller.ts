4996238f6ec1638ca88cee9e6492c857382a5407
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2025-10-02 20:32:24 -0500

    feat: update message patterns for login and verify user commands in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,19 +6,22 @@ import { MessagePattern } from '@nestjs/microservices'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /*Poner el pattern en un MessagePattern, significa que este microservicio
+  está suscribiendose a {cmd:'auth.register.user}
+  */
   @MessagePattern({cmd:'auth.register.user'})
   register(){
     return 'Registrado'
   }
 
-  @MessagePattern({cmd:''})
+  @MessagePattern({cmd:'auth.login.user'})
   login(){
-
+    return 'Login'
   }
 
-  @MessagePattern({cmd:''})
+  @MessagePattern({cmd:'auth.verify.user'})
   verify(){
-    
+    return 'Verify'
   }
 }
 
